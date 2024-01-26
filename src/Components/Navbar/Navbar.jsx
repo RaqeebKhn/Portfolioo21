c9425a343bd5d91972e90062213b8722817d8963
RaqeebKhn <joysouls.rk@gmail.com>
2024-01-26 23:53:21 +0530

    mobileNav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import MobileNav from './MobileNav/MobileNav.jsx';
 
 const Navbar = () => {
+    const [openMenu, setOpenMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setOpenMenu(!openMenu);
+    };
+
   return (
+    <>
+    
+    <MobileNav isOpen={openMenu} toggleMenu={toggleMenu} />
+    
+    
     <nav className='nav-wrapper'>
         <div className='nav-content'>
             <img className='logo' src='./Images/icon1.svg' alt=''></img>
@@ -24,17 +36,18 @@ const Navbar = () => {
                 </button>
             </ul>
 
-            <button class="menu-btn" onClick={() => {}}>
+            <button className="menu-btn" onClick={toggleMenu}>
                 <span 
                     class={"material-symbol-outlined"}
                     style={{fontsize:"1.8rem"}}
                 >
-                    = 
+                    {openMenu ? "x" : "="}
                 </span>
             </button>
         </div>
     </nav>
-  )
+    </>
+  )  
 }
 
 export default Navbar
