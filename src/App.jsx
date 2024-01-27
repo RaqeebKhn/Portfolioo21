126d7c4f5ec212b3f705222f35595a88c84c0f1d
RaqeebKhn <joysouls.rk@gmail.com>
2024-01-28 00:02:32 +0530

    Hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
 
 function App() {
   return (
     <>
       <Navbar />
+
+      <div className='container'>
+        <Hero />
+      </div>
+      
     </>
   );
 }
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <section className='hero-container'>
+        <div className='hero-content'>
+            <h2>Building Digital Experiences That Inspire</h2>
+            <p>
+                Passionate Frontend Developer | Transforming Ideas Into Seamless and Visually Stunning Web Solutions
+            </p>
+        </div>
+
+        <div className='hero-img'>
+            <div>
+                <div className='tech-icon'>
+                    <img src="./Images/icons8-react-native.svg" alt="" />
+                </div>
+                    <img className="rr" src="./Images/dp1.jpg" alt="" />
+            </div>
+                
+            <div>
+                <div className='tech-icon'>
+                    <img src="./Images/icons8-html.svg" alt="" />
+                </div>
+                <div className='tech-icon'>
+                    <img src="./Images/icons8-css.svg" alt="" />
+                </div>
+                <div className='tech-icon'>
+                    <img src="./Images/icons8-js.svg" alt="" />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero;
